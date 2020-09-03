ccead3b7e57a7b8e8e5309f477e94e6e4f5e6e21
Ahmed Farag <ahmedfarag1993@gmail.com>
2020-09-03 16:09:21 +0200

    ff

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -6,41 +6,19 @@ const {
   DB_NAME,
 } = require('./env.config');
 
-const db_config_postgresql = {
-  HOST: DB_HOST,
-  PORT: DB_PORT,
-  USER: DB_USER,
-  PASSWORD: DB_PASSWORD,
-  DB: DB_NAME,
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
-
-const db_config_mysql = {
-  HOST: DB_HOST,
-  PORT: DB_PORT,
-  USER: DB_USER,
-  PASSWORD: DB_PASSWORD,
-  DB: DB_NAME,
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
-
 const db_config = {
-  dev: db_config_mysql,
-  production: db_config_postgresql,
+  HOST: DB_HOST,
+  PORT: DB_PORT,
+  USER: DB_USER,
+  PASSWORD: DB_PASSWORD,
+  DB: DB_NAME,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
 
-const env = process.env.NODE_ENV;
-
-module.exports = db_config[env];
+module.exports = db_config;
