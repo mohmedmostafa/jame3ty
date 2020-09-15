976a511c5c83f8024e37365ca4e63d10f3d5f088
Ahmed Farag <ahmedfarag1993@gmail.com>
2020-09-15 05:14:05 +0200

    new tables

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.listen(process.env.PORT || PORT, () => {
 //ٍSync DB Tables according to Models
 //force: true will drop the table if it already exists
 
-if (0) {
+if (1) {
   db.connection
     .query('SET FOREIGN_KEY_CHECKS = 0', null, { raw: true })
     .then(async (result) => {
